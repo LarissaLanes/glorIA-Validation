f34492bf2ece40ba0a18fc91f521eed85c8be96e
Your Name <larissalanes@Larissas-MacBook-Air.local>
2024-01-26 18:41:19 -0300

    configViewCode

---
+++ gloria/gloria/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  gloria
+//
+//  Created by Larissa Lanes on 26/01/24.
+//
+
+import UIKit
+import OpenAISwift
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+     
+    }
+
+
+}
